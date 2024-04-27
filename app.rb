6eb156576e2121e20da6b6a5543d4441a18f9044
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-04-27 17:03:41 +0000

    add form and result

--- app.rb
+++ app.rb
@@ -5,12 +5,13 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
   @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
 
   @result = @first_num + @second_num
   erb(:add_result)
@@ -24,21 +25,43 @@ get("/wizard_subtract") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
 
+  @result = @second_num - @first_num
+
   erb(:sub_result)
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num * @second_num
+
   erb(:mult_result)
 end
 
 get("/divide") do
-  erb(:div_form)
+  erb(:divform)
 end
 
 get("/wizard_divide") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num / @second_num
+  if @second_num != 0
+    if (@first_num % @second_num) == 0
+      puts @first_num / @second_num
+    else
+      @result = @first_num / @second_num
+      @formatted_result = sprintf("%.16f", @result)
+      puts @formatted_result
+    end
+  else
+    puts "Infinity"
+  end
   erb(:div_result)
 end
